b8826eb78aed24efd51063b82fb172b9657e899f
moyanxiaoq <374561129@qq.com>
2020-04-08 00:05:56 +0800

    添加一些可以自定义的样式配置

--- .vuepress/theme/index.js
+++ .vuepress/theme/index.js
@@ -6,40 +6,46 @@ module.exports = {
     ['vuepress-plugin-code-copy', true],// 暂时没起作用
     // see: https://vssue.js.org/guide/vuepress.html#usage
     ['@vssue/vuepress-plugin-vssue', { // 暂时未配置
-      platform: 'github',
-      owner: 'moyanxiaoq',
-      repo: 'xxxxxxxx',
-      clientId: 'xxxxxxxxxxx',
-      clientSecret: 'xxxxxxxxxxxxxxxxxxxxxx',
-    }],
+        platform: 'github',
+        owner: 'moyanxiaoq',
+        repo: 'xxxxxxxx',
+        clientId: 'xxxxxxxxxxx',
+        clientSecret: 'xxxxxxxxxxxxxxxxxxxxxx',
+      }
+    ],
     // see: https://vuepress.github.io/zh/plugins/copyright/#%E5%AE%89%E8%A3%85
     ['copyright', { // 暂时没起作用
-      noCopy: false, // 允许复制内容
-      minLength: 10, // 如果长度超过 100 个字符
-      authorName: "https://moyanxiaoq.github.io/",
-      // clipboardComponent: "请注明文章出处, [快乐学习与分享](https://moyanxiaoq.github.io/)"
-    }],
-    // See: https://vuepress.github.io/zh/plugins/medium-zoom/#%E5%AE%89%E8%A3%85
-    ['vuepress-plugin-medium-zoom', {
-      selector: 'img',
-      delay: 1000,
-      options: {
-        margin: 16,
-        background: '#ccc',
-        scrollOffset: 0,
+        noCopy: false, // 允许复制内容
+        minLength: 10, // 如果长度超过 100 个字符
+        authorName: "https://moyanxiaoq.github.io/",
+        // clipboardComponent: "请注明文章出处, [快乐学习与分享](https://moyanxiaoq.github.io/)"
       }
-    }],
+    ],
+    // See: https://vuepress.github.io/zh/plugins/medium-zoom/#%E5%AE%89%E8%A3%85
+    ['vuepress-plugin-medium-zoom', 
+      {
+        selector: 'img',
+        delay: 1000,
+        options: {
+          margin: 16,
+          background: '#ccc',
+          scrollOffset: 0,
+        }
+      }
+    ],
     ['vuepress-plugin-nprogress', {}],
     // See: https://vuepress.github.io/zh/plugins/container/#vuepress-plugin-container
     ['vuepress-plugin-container', {
         type: 'right',
         defaultTitle: '',
-      }],
+      }
+    ],
     ['vuepress-plugin-container', {
         type: 'theorem',
         before: info => `<div class="theorem"><p class="title">${info}</p>`,
         after: '</div>',
-      }],
+      }
+    ],
     // 这是 VuePress 默认主题使用这个插件的方式
     ['vuepress-plugin-container', {
         type: 'tip',
@@ -47,6 +53,13 @@ module.exports = {
           '/': 'TIP',
           '/zh/': '提示',
         },
-      }],
+      }
+    ],
+    ['vuepress-plugin-container', 
+      {
+        type: 'czq',
+        defaultTitle: '',
+      }
+    ],
   ],
 }
---
+++ page/Readme.md
@@ -0,0 +1,28 @@
+---
+title: page页面测试
+lang: zh-CN
+meta:
+  - name: description
+    content: page页面测试
+  - name: keywords
+    content: VuePress Vuepress心血来潮
+tags: 
+  - page
+  - VuePress
+date: 2020-04-07
+author: chenzhiq
+location: 南京
+summary:  page页面测试
+---
+
+## java
+
+> 落霞与孤鹜齐飞，秋水共长天一色。
+
+## python
+
+> 人生苦短我用python。
+
+::: theorem bar
+随便写点啥 ~
+:::
